e3581b7a69aa1f082a2c651cf7ec99dbfdbe213d
ishu9bansal <ishubansal1400@gmail.com>
2021-03-06 23:42:17 +0530

    add chart to show arr values

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const boxHeight = 20;
 const boxWidth = 50;
 const period = 1000;
 const quick = 200;
-const layers = 2;
+const layers = 3;
+const chartHeight = 300;
+const chartWidth = 400;
+var resizeFactor;
+var chart;
 var limit;
 var mapping;
 var svg;
@@ -12,9 +16,18 @@ var root;
 var width;
 var height;
 var layer;
+var values;
+var barWidth;
+var barHeight;
 
 function set(i,x){
 	if(i>limit||i<=0)	return;
+	var temp = values.find(d => d.id == i);
+	if(!temp)	values.push(temp = {
+		id: i,
+		value: 0
+	});
+	temp.value += x;
 	arr = [];
 	while(i<=limit){
 		arr.push(i.toString());
@@ -30,6 +43,7 @@ function set(i,x){
 	}
 	var trans_period = transitionAnimation();
 	setTimeout(() => highlight(arr), trans_period);
+	renderChart();
 }
 
 function transitionAnimation(){
@@ -127,7 +141,42 @@ function randomAdd(){
 	set(i,x);
 }
 
+function renderChart(){
+	maxValue = 0;
+	values.forEach(d => maxValue = Math.max(maxValue, Math.abs(d.value)));
+	barHeight = (chartHeight/2-barWidth)/(maxValue||1);
+
+	chart.transition().duration(quick).style('opacity', resizeFactor)
+	.attr('transform', 'translate('+(width-offset-chartWidth*resizeFactor)+','+(height-offset-chartHeight*resizeFactor/2)+')');
+	chart.select('rect.chart').transition().duration(quick)
+	.attr('x', 0).attr('y', -chartHeight*resizeFactor/2)
+	.attr('height', chartHeight*resizeFactor)
+	.attr('width', chartWidth*resizeFactor);
+	chart.select('line.chart').transition().duration(quick)
+	.attr('x1', barWidth*resizeFactor).attr('y1', 0)
+	.attr('x2', (chartWidth-barWidth)*resizeFactor).attr('y2', 0);
+
+	chart.selectAll('rect.bar')
+	.data(values, d => d.id)
+	.enter().append('rect')
+	.classed('bar', true)
+	.attr('x', d => d.id*barWidth*resizeFactor)
+	.attr('y', 0).attr('height', 0)
+	.attr('width', barWidth*resizeFactor)
+	.style('fill', d => d.value>0?'cyan':'pink');
+
+	chart.selectAll('rect.bar')
+	.transition().duration(quick)
+	.attr('x', d => d.id*barWidth*resizeFactor)
+	.attr('y', d => d.value<0?0:-d.value*barHeight*resizeFactor)
+	.attr('height', d => Math.abs(d.value*barHeight*resizeFactor))
+	.attr('width', barWidth*resizeFactor)
+	.style('fill', d => d.value>0?'cyan':'pink');
+
+}
+
 function init(){
+	resizeFactor = 0.5;
 	limit = 1<<treeHeight;
 	width = window.innerWidth - 2*offset;
 	height = window.innerHeight - 2*offset;
@@ -143,7 +192,24 @@ function init(){
 	for(var i = 0; i < layers; i++){
 		layer.push(svg.append('g'));
 	}
+
+	barWidth = chartWidth/(limit+2);
+	chart = layer[2].append('g').classed('chart', true)
+	.attr('transform', 'translate('+(width-offset-chartWidth*resizeFactor)+','+(height-offset-chartHeight*resizeFactor/2)+')')
+	.style('opacity', resizeFactor);
+
+	chart.append('rect').classed('chart', true)
+	.attr('x', 0).attr('y', 0).attr('height', 0).attr('width', 0)
+	.style('fill', 'grey').style('opacity', 0.5);
+	chart.append('line').classed('chart', true)
+	.attr('x1', 0).attr('y1', 0).attr('x2', 0).attr('y2', 0)
+	.style('stroke', 'black');
+
+	values = [];
+
 	mapping = [];
+
+	renderChart();
 }
 
 init();
